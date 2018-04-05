bece4169f70e2289606f1e9e2eac02ee2fa88bac
an-schneider <35575344+an-schneider@users.noreply.github.com>
2018-04-04 21:30:19 -0400

    add functioning App.js

--- hrm-client/src/App.js
+++ hrm-client/src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import Button from 'material-ui/Button';
+import TextField from 'material-ui/TextField'
+import ClientTextField from './ClientTextField.js';
+import axios from 'axios';
+import FetchData from './GetData.js'
 
 class App extends Component {
   render() {
     return (
         <div>
-            <Button variant="raised">
-                Search
-            </Button>
+            <FetchData/>
         </div>
     );
   }
